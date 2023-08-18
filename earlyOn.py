067102907743a66341d89a13b0c310ede015b057
jacobwdv <jacobwdv@ca.ibm.com>
2023-08-18 10:46:24 -0400

    Update earlyOn.py
    sorting by date, fixing query, adding description field, filtering by active.

--- earlyOn.py
+++ earlyOn.py
@@ -2,7 +2,6 @@ import webbrowser
 import requests
 from tabulate import tabulate
 import datetime
-from pretty_html_table import build_table
 from bs4 import BeautifulSoup
 
 
@@ -11,21 +10,11 @@ def unix_time_millis(dt):
     return (dt - epoch).total_seconds() * 1000.0
 
 
-# The API endpoint
-url = "https://www.keyon.ca/en/Events/GetEvents?id=501&startDate=2023-07-30T04%3A00%3A00.000Z&endDate=2023-09-10T04%3A00%3A00.000Z"
-
-# A GET request to the API
-response = requests.get(url)
-
-# Print the response
-response_json = response.json()
-
-table = [['Start Time', 'End Time', 'Name']]
-
-
 def get_from_utc_time(epoch_time):
     date = datetime.datetime.fromtimestamp(epoch_time)
-    return date
+    date_str = date.strftime("%B %d, %Y")
+    time_str = date.strftime("%I:%M%p")
+    return date_str, time_str
 
 
 def get_epoch_time_from_earlyon_format(time_str):
@@ -56,11 +45,41 @@ def make_tables_pretty(html_content):
     return soup.prettify()
 
 
-for item in response_json:
+today = datetime.datetime.now()
+thirty_days_from_today = today + datetime.timedelta(30)
+
+# The API endpoint
+url_time_format = "%Y-%m-%d"
+url = "https://www.keyon.ca/en/Events/GetEvents?id=501&startDate={start_date}T04%3A00%3A00.000Z&endDate={end_date}T04%3A00%3A00.000Z".format(
+    start_date=today.strftime(url_time_format), end_date=thirty_days_from_today.strftime(url_time_format))
+
+print(url)
+# A GET request to the API
+response = requests.get(url)
+
+# Print the response
+response_json = response.json()
+
+filtered_response_json = [
+    item for item in response_json if item['IsActive'] is True]
+
+sorted_response_json = sorted(filtered_response_json, key=lambda x: x['start'])
+table = [['Date', 'Start Time', 'End Time', 'Name', 'Description']]
+
+
+def get_description(item):
+    soup = BeautifulSoup(item['description'], features='html.parser')
+    return soup.getText()
+
+
+for item in sorted_response_json:
     start = get_epoch_time_from_earlyon_format(item['start'])
     end = get_epoch_time_from_earlyon_format(item['end'])
-    table.append([str(get_from_utc_time(start)), str(
-        get_from_utc_time(end)), item['title']])
+    start_date, start_time = get_from_utc_time(start)
+    _, end_time = get_from_utc_time(end)
+    description = get_description(item)
+    table.append([start_date, start_time, end_time,
+                 item['title'], description])
 
 with open('earlyOn.html', 'w') as html_file:
     table = tabulate(table, tablefmt='html', headers='firstrow')
